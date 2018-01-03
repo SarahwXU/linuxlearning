0009d9e0a57f017b414f0cd147dbc3ce687dd786
SarahwXU <bowen.xu.whu@gmail.com>
2018-01-03 19:44:27 +0800

    Receive attachment according to subject

--- s_r_email.py
+++ s_r_email.py
@@ -10,6 +10,8 @@ from smtplib import SMTP_SSL
 import poplib
 # decode Chinese character from windows system
 import base64
+# 
+from os import path
 
 def send_mail(sender_qq='',pwd='', receiver='',mail_title='',mail_content=''):
 	# stmp server of qq mail
@@ -49,7 +51,7 @@ def decode_str(s):
 		value = value.decode(charset)
 	return value
 
-def receive_mail(receiver_qq='', pwd=''):
+def receive_mail(receiver_qq='', pwd='', subject_define='', filepath=''):
 	host_server = 'pop.qq.com'
 	receiver_qq_mail = receiver_qq+'@qq.com'
 
@@ -61,61 +63,71 @@ def receive_mail(receiver_qq='', pwd=''):
 
 	print('Messages: %s. Size: %s' % pop_conn.stat())
 	# get the latest email
-	response, listings, octets = pop_conn.list()
-#	lines = pop_conn.retr(1)[1]
-	print(len(listings))
-#	print(listings)
-#	print(lines)
-#	for i in range(1,len(listings)+1):
-	i = len(listings);	
-	resp, lines, octe = pop_conn.retr(i)
-#	print(lines)
-#	print(len(lines))
-#	for line in lines: 
-#		message = email.message_from_bytes(line)
-#		print(message)
-	message = email.message_from_bytes(b'\n'.join(lines))
+#	response, listings, octets = pop_conn.list()
+#	i = len(listings);	
+#	resp, lines, octe = pop_conn.retr(i)
+#	message = email.message_from_bytes(b'\n'.join(lines))
+
+	lines = [pop_conn.retr(i) for i in range(1,len(pop_conn.list()[1]) + 1)]
+#	messages = [email.message_from_bytes(b'\n'.join(line[1]) for line in lines)]
+	for line in lines:
+		message = email.message_from_bytes(b'\n'.join(line[1]))
 # depend on different mail box
 #	message = email.message_from_string('\n'.join(listings[1]))
 
-	for header in ['From', 'To', 'Subject', 'Date']:
-		value = message.get(header, '')
-		if value:
-			if header == 'Subject':
-				value = decode_str(value)
+# print email title 
+#	for header in ['From', 'To', 'Subject', 'Date']:
+#		value = message.get(header, '')
+#		if value:
+#			if header == 'Subject':
+#				value = decode_str(value)
+#			else:
+#				hdr, addr = parseaddr(value)
+#				name = decode_str(hdr)
+#				value = u'%s <%s>' % (name, addr)
+#		print('%s%s: %s' % ('  ' * 0, header, value))
+
+		subject_value = message.get('Subject') 
+		subject_name = decode_str(subject_value)
+		if isinstance(subject_name, str):
+			if 'summary' in subject_name:
+				for part in message.walk():
+					if not part.is_multipart():
+		
+						fileName = part.get_filename()
+						charset = part.get_content_charset()
+#						content = part.get_payload(decode=True)
+				
+						if fileName:
+							h = email.header.Header(fileName)
+							dh = email.header.decode_header(h)
+							filename = dh[0][0]
+#							print(filename)
+#							print(dh[0][1])
+							filenamestr = bytes.decode(filename)
+							if filenamestr[0] == '=':
+								# get the encode mode
+								s = filename.split(b'?')
+								finalname = base64.b64decode(s[3]).decode(bytes.decode(s[1]))
+							else:
+								finalname = filenamestr
+#							print(finalname)
+						# get email content
+#							contentType = part.get_content_type()
+						# get attachment				
+							data = part.get_payload(decode=True)
+							if path.exists(filepath):
+								if not path.isfile(filepath+finalname):
+									fEx = open(finalname, 'wb')
+									fEx.write(data)
+									fEx.close()
+								else:
+									print(finalname+' is already exists')
+							else:
+								print('The saving path is not exists !') 
 			else:
-				hdr, addr = parseaddr(value)
-				name = decode_str(hdr)
-				value = u'%s <%s>' % (name, addr)
-		print('%s%s: %s' % ('  ' * 0, header, value))
-
-	for part in message.walk():
-		if not part.is_multipart():
-
-			fileName = part.get_filename()
-			charset = part.get_content_charset()
-#			content = part.get_payload(decode=True)
-			
-			if fileName:
-				h = email.header.Header(fileName)
-				dh = email.header.decode_header(h)
-				filename = dh[0][0]
-#				print(filename)
-#				print(dh[0][1])
-				filenamestr = bytes.decode(filename)
-				if filenamestr[0] == '=':
-				# get the encode mode
-					s = filename.split(b'?')
-					finalname = base64.b64decode(s[3]).decode(bytes.decode(s[1]))
-				else:
-					finalname = filenamestr
-				print(finalname)
-				# get email content
-#				contentType = part.get_content_type()
-				# get attachment				
-				data = part.get_payload(decode=True)
-				fEx = open(finalname, 'wb')
-				fEx.write(data)
-				fEx.close()
+				pass
+		else:
+			pass
 				
 	pop_conn.quit() 
